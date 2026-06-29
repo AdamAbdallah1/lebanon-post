b2df2841f7fdffec8dac35f361a7c3f1804eb62f
Adam Abdallah <adamabdallahayln1@gmail.com>
2026-06-29 04:49:16 +0300

    Fixing base

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { LanguageProvider } from "./context/LanguageContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/lebanon-post">
       <LanguageProvider>
         <ThemeProvider>
           <App />
